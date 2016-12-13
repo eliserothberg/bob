af6a4228aa23fbb608a27c9234e134ef99815c34
eliserothberg <rothberg.elise@gmail.com>
2016-12-13 11:49:37 -0800

    jaws

--- connection/connection.js
+++ connection/connection.js
@@ -1,21 +1,25 @@
-/*
-Here is where you make the connection to the database and export
-*/
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: 'password',
-	database: 'sales_db'
-});
+var connection;
 
-connection.connect(function (err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'sales_db'
+  });
+};
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+
+  console.log('connected as id ' + connection.threadId);
+
 });
 
 module.exports = connection;
