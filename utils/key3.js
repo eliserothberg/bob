1ec3377e9620e52123a060f258f3129776d88b17
eliserothberg <rothberg.elise@gmail.com>
2016-12-14 01:07:20 -0800

    rr

--- utils/key3.js
+++ utils/key3.js
@@ -1,3 +1,8 @@
-var auth = process.env.authKEY;
-console.log("key.js auth key = " + auth);
+var auth = 'Basic NTgwN2IyYmFlNGIwOWQzYjc1ZmMwMjU4OkUxaFZCc0ozQ1dPS0hnR3V6Rkx1NGNZTHkyNA==';
+
 module.exports = auth;
+
+
+// var auth = process.env.authKEY;
+// console.log("key.js auth key = " + auth);
+// module.exports = auth;
