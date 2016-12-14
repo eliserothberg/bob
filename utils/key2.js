85bc93077a126c966435793060d40b6671832abc
eliserothberg <rothberg.elise@gmail.com>
2016-12-14 02:28:13 -0800

    again

---
+++ utils/key2.js
@@ -0,0 +1,22 @@
+var auth = require('../utils/key4.js');
+var request = require("request");
+var Promise = require('bluebird');
+
+function getList () {
+  var theList = { method: 'GET',
+    url: 'https://api.salesforceiq.com/v2/lists/57114e10e4b0a3f93805ebc6',
+    headers: 
+     { 
+       'Authorization': auth,
+       'Content-type': 'application/json'
+     },
+   
+    json: true
+  };
+
+  return Promise.promisify(request)(theList)
+  .then(function(res) {
+    return res.body;
+  });
+};
+module.exports = getList;
