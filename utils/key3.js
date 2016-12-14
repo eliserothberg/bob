94341a9d08157910b49d9e582a449a263b29dbb7
eliserothberg <rothberg.elise@gmail.com>
2016-12-14 12:57:19 -0800

    change sameple data

--- utils/key3.js
+++ utils/key3.js
@@ -2,3 +2,6 @@ var auth = process.env.auth_key;
 
 console.log("key.js auth key = " + auth);
 module.exports = auth;
+
+//only operate on the JSON but download it with keys
+// one test file per source file do a file per test
